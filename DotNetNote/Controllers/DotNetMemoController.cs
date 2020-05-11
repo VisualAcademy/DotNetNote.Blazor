a8b6ab5a73a453c72e3b355c294a89abffff342c
RedPlus <redplus@redplus.net>
2020-05-11 19:13:57 +0000

    DotNetMemo 컨트롤러 클래스와 여러 액션 메서드를 생성하고 관련 뷰페이지 생성

---
+++ DotNetNote/Controllers/DotNetMemoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Controllers
+{
+    public class DotNetMemoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create() => View();
+
+        [HttpGet]
+        public IActionResult Details() => View();
+
+        [HttpGet]
+        public IActionResult Edit() => View();
+
+        [HttpGet]
+        public IActionResult Delete() => View();
+
+        [HttpGet]
+        public IActionResult Reply() => View();
+    }
+}
