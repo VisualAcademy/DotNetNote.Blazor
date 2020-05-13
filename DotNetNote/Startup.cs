e9e5405b810fdef5c8f85314d37deaebc62d108e
RedPlus <redplus@redplus.net>
2020-05-13 22:18:37 +0000

    ASP.NET Core를 다루는 기술 책 37장 소스 적용하기

--- DotNetNote/Startup.cs
+++ DotNetNote/Startup.cs
@@ -1,5 +1,7 @@
-using DotNetNote.Areas.Identity;
+﻿using DotNetNote.Areas.Identity;
 using DotNetNote.Data;
+using DotNetNote.Models;
+using DotNetNote.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
@@ -32,6 +34,50 @@ namespace DotNetNote
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddSingleton<WeatherForecastService>();
+
+            //[!] Configuration: JSON 파일의 데이터를 POCO 클래스에 주입
+            services.Configure<DotNetNoteSettings>(Configuration.GetSection("DotNetNoteSettings"));
+
+            // ============================================================================== // 
+            // 새로운 DbContext 추가
+            services.AddEntityFrameworkSqlServer().AddDbContext<DotNetNoteContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            // ============================================================================== // 
+
+            //[DI] 의존성 주입(Dependency Injection)
+            DependencyInjectionContainer(services);
+        }
+
+        /// <summary>
+        /// 의존성 주입 관련 코드만 따로 모아서 관리
+        /// - 리포지토리 등록
+        /// </summary>
+        private void DependencyInjectionContainer(IServiceCollection services)
+        {
+            //[?] ConfigureServices가 호출되기 전에는 DI(종속성 주입)가 설정되지 않습니다.
+
+            //[DNN][!] Configuration 개체 주입: 
+            //    IConfiguration 또는 IConfigurationRoot에 Configuration 개체 전달
+            //    appsettings.json 파일의 데이터베이스 연결 문자열을 
+            //    리포지토리 클래스에서 사용할 수 있도록 설정
+            // IConfiguration 주입 -> Configuration의 인스턴스를 실행 
+            services.AddSingleton<IConfiguration>(Configuration);
+
+            //[DNN][1] 게시판 관련 서비스 등록            
+            //[1] 생성자에 문자열로 연결 문자열 지정
+            //services.AddSingleton<INoteRepository>(
+            //  new NoteRepository(
+            //      Configuration["Data:DefaultConnection:ConnectionString"]));            
+            //[2] 의존성 주입으로 Configuration 주입
+            //[a] NoteRepository에서 IConfiguration으로 데이터베이스 연결 문자열 접근
+            services.AddTransient<INoteRepository, NoteRepository>();
+            //[b] CommentRepository의 생성자에 데이터베이스 연결문자열 직접 전송
+            //services.AddSingleton<INoteCommentRepository>(
+            //    new NoteCommentRepository(
+            //        Configuration["ConnectionStrings:DefaultConnection"]));
+            //[b] 위 코드를 아래 코드로 변경
+            services.AddTransient<INoteCommentRepository, NoteCommentRepository>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
