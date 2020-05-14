2b0c9cbecd7562663be7d4d5ead6bd0aa4c12e43
RedPlus <redplus@redplus.net>
2020-05-14 17:33:45 +0000

    기본 계정과 역할 그리고 역할 기반 인증 방식으로 변경

--- DotNetNote/Startup.cs
+++ DotNetNote/Startup.cs
@@ -1,15 +1,20 @@
 ﻿using DotNetNote.Areas.Identity;
 using DotNetNote.Data;
 using DotNetNote.Models;
+using DotNetNote.Services;
 using DotNetNote.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DotNetNote
 {
@@ -27,8 +32,8 @@ namespace DotNetNote
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
             services.AddControllersWithViews(); // MVC 3.1
             services.AddRazorPages();
             services.AddServerSideBlazor();
@@ -44,6 +49,8 @@ namespace DotNetNote
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             // ============================================================================== // 
+
+            services.AddTransient<IEmailSender, EmailSender>();
 
             //[DI] 의존성 주입(Dependency Injection)
             DependencyInjectionContainer(services);
@@ -80,7 +87,7 @@ namespace DotNetNote
             services.AddTransient<INoteCommentRepository, NoteCommentRepository>();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -111,6 +118,115 @@ namespace DotNetNote
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
+
+            CreateBuiltInUsersAndRoles(serviceProvider).Wait();
+        }
+
+        private async Task CreateBuiltInUsersAndRoles(IServiceProvider serviceProvider)
+        {
+            //[0] DbContext 개체 생성
+            var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+            dbContext.Database.EnsureCreated(); // 데이터베이스가 생성되어 있는지 확인 
+
+            // 기본 내장 사용자 및 역할이 하나도 없으면(즉, 처음 데이터베이스 생성이라면)
+            if (!dbContext.Users.Any() && !dbContext.Roles.Any())
+            {
+                string domainName = "a.com";
+
+                //[1] Groups(Roles)
+                var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                //[1][1] ('Administrators', '관리자 그룹', 'Group', '응용 프로그램을 총 관리하는 관리 그룹 계정')
+                //[1][2] ('Everyone', '전체 사용자 그룹', 'Group', '응용 프로그램을 사용하는 모든 사용자 그룹 계정')
+                //[1][3] ('Users', '일반 사용자 그룹', 'Group', '일반 사용자 그룹 계정')
+                //[1][4] ('Guests', '관리자 그룹', 'Group', '게스트 사용자 그룹 계정')
+                string[] roleNames = { "Administrators", "Everyone", "Users", "Guests", "Managers" };
+                foreach (var roleName in roleNames)
+                {
+                    var roleExist = await roleManager.RoleExistsAsync(roleName);
+                    if (!roleExist)
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(roleName)); // 빌트인 그룹 생성
+                    }
+                }
+
+                //[2] Users
+                var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                //[2][1] Administrator
+                // ('Administrator', '관리자', 'User', '응용 프로그램을 총 관리하는 사용자 계정')
+                IdentityUser administrator = await userManager.FindByEmailAsync($"administrator@{domainName}");
+                if (administrator == null)
+                {
+                    administrator = new IdentityUser()
+                    {
+                        UserName = $"administrator@{domainName}",
+                        Email = $"administrator@{domainName}",
+                        EmailConfirmed = true,
+                    };
+                    await userManager.CreateAsync(administrator, "Pa$$w0rd");
+                }
+
+                //[2][2] Guest
+                // ('Guest', '게스트 사용자', 'User', '게스트 사용자 계정')
+                IdentityUser guest = await userManager.FindByEmailAsync($"guest@{domainName}");
+                if (guest == null)
+                {
+                    guest = new IdentityUser()
+                    {
+                        UserName = "Guest",
+                        Email = $"guest@{domainName}",
+                    };
+                    await userManager.CreateAsync(guest, "Pa$$w0rd");
+                }
+
+                //[2][3] Anonymous
+                // ('Anonymous', '익명 사용자', 'User', '익명 사용자 계정')
+                IdentityUser anonymous = await userManager.FindByEmailAsync($"anonymous@{domainName}");
+                if (anonymous == null)
+                {
+                    anonymous = new IdentityUser()
+                    {
+                        UserName = "Anonymous",
+                        Email = $"anonymous@{domainName}",
+                    };
+                    await userManager.CreateAsync(anonymous, "Pa$$w0rd");
+                }
+
+                //[2][4] User
+                // ('User', '일반사용자', 'User', '응용 프로그램에 로그인할 수 있는 사용자')
+                IdentityUser user = await userManager.FindByEmailAsync($"user@{domainName}");
+                if (user == null)
+                {
+                    user = new IdentityUser()
+                    {
+                        UserName = $"user@{domainName}",
+                        Email = $"user@{domainName}",
+                        EmailConfirmed = true,
+                    };
+                    await userManager.CreateAsync(user, "Pa$$w0rd");
+                }
+
+                //[2][5] Manager
+                // ('User', '일반사용자', 'User', '응용 프로그램에 로그인할 수 있는 사용자')
+                IdentityUser manager = await userManager.FindByEmailAsync($"manager@{domainName}");
+                if (manager == null)
+                {
+                    manager = new IdentityUser()
+                    {
+                        UserName = $"manager@{domainName}",
+                        Email = $"manager@{domainName}",
+                        EmailConfirmed = true,
+                    };
+                    await userManager.CreateAsync(manager, "Pa$$w0rd");
+                }
+
+                //[3] UsersInRoles: AspNetUserRoles Table
+                await userManager.AddToRoleAsync(administrator, "Administrators");
+                await userManager.AddToRoleAsync(administrator, "Users");
+                await userManager.AddToRoleAsync(guest, "Guests");
+                await userManager.AddToRoleAsync(anonymous, "Guests");
+                await userManager.AddToRoleAsync(user, "Users");
+                await userManager.AddToRoleAsync(manager, "Managers");
+            }
         }
     }
 }
